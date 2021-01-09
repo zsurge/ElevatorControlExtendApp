81477c2ec8354dfb591d6c148275a4070fade99f
zhangduo <zdsurge@163.com>
2021-01-09 10:14:02 +0800

    V1.0.3 修正8楼以上二维码无法自动呼梯的BUG

--- 4UserLayer/Task/HandShake_Task.c
+++ 4UserLayer/Task/HandShake_Task.c
@@ -107,7 +107,7 @@ static void vTaskHandShake(void *pvParameters)
 
     while(1)
     {  
-        bsp_HC595Show('A',0,2);
+        bsp_HC595Show('A',0,3);
         vTaskDelay(800);
         bsp_HC595Show(0,0,1);
         vTaskDelay(800);
